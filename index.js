a4102c4f9e96350935d25a198cc09412a9460526
KEVIN CHAU <kevinch@udel.edu>
2024-01-08 17:00:18 -0500

    Refactored logic for handling clicks

--- index.js
+++ index.js
@@ -1,33 +1,18 @@
-function createPlayer(name, symbol){
-    const playerName = name;
-    const playerSymbol = symbol;
-    let getPlayerName = () => {
-        console.log('The player name is ' + playerName + ' and their symbol is: ' + playerSymbol )
-    }
-
-    return {playerName, playerSymbol,getPlayerName};
-}
-
-function handleClick(symbol) {
+function clickHandler(symbol){
     let currSymbol = symbol;
-    let squares = document.querySelectorAll('.board-square')
-    console.log(squares);
-
-    for (let i = 0; i < squares.length; i++){
-        squares[i].addEventListener('click', () => {
+    let squares = document.querySelectorAll('[data-cell]');
+    squares.forEach((square) => {
+        square.addEventListener('click', () => {
             if (currSymbol === 'X'){
-                squares[i].textContent = currSymbol;
+                square.textContent = currSymbol;
                 currSymbol = 'O';
-                console.log(currSymbol);
             }
-            else if (currSymbol === 'O'){
-                squares[i].textContent = currSymbol;
+            else{
+                square.textContent = currSymbol;
                 currSymbol = 'X';
             }
-        },
-        {once: true}) //ensures that you can only click each square once
-
-    }
+        }
+        )})
 }
 
-handleClick('X');
+clickHandler('X');
